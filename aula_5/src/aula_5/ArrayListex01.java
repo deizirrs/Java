ee2824edaea1c52a113083d4e3336842a284ef31
deizirrs <deizianesantos.rodrigues15@gmail.com>
2023-03-23 02:45:30 -0300

    Aula_5

---
+++ aula_5/src/aula_5/ArrayListex01.java
@@ -0,0 +1,36 @@
+package aula_5;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+public class ArrayListex01 {
+
+	 public static void main(String[] args) {
+
+		 Scanner leia = new Scanner(System.in);
+		 {
+				ArrayList<String> listaCor = new ArrayList<>();
+				
+				
+				System.out.print("Digite a 1º cor: ");
+				listaCor.add(leia.next());
+				System.out.print("Digite a 2º cor: ");
+				listaCor.add(leia.next());
+				System.out.print("Digite a 3º cor: ");
+				listaCor.add(leia.next());
+				System.out.print("Digite a 4º cor: ");
+				listaCor.add(leia.next());
+				System.out.print("Digite a 5º cor: ");
+				listaCor.add(leia.next());
+				
+				System.out.println(listaCor);
+				
+				listaCor.sort(Comparator.naturalOrder()); 
+				System.out.println("Lista de cores ordenadas: " + listaCor);
+			}
+	        
+	    }
+
+	}
